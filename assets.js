3861fbb07c757fdd2db713fa421284975757007b
Indiana.json <1751044+indianajson@users.noreply.github.com>
2025-05-28 21:53:35 -0400

    beta test

---
+++ assets.js
@@ -0,0 +1,164 @@
+let allData = [
+      {
+            "name": "Den Area Tiles",
+            "files": [
+                  "BN2_ACDC.tsx",
+                  "BN2_ACDC.png"
+            ],
+            "author": "Quetzen",
+            "type": "tile",
+            "credit": true,
+            "game": "BN2",
+            "preview": "BN2_ACDC.png"
+      },
+      {"name":"Den Area Floor Square","files":["ACDC_Net_Tileset.png","ACDC_Net_Tileset.tsx"],"type":"tile","author":"unknown","credit":false,"game":"BN2","preview":"ACDC_Net_Tileset.png"},
+
+      
+      {
+            "name": "Security Cube",
+            "files": [
+                  "bn6_securitycube.tsx",
+                  "bn6_securitycube.png"
+            ],
+            "author": "unknown",
+            "type": "object",
+            "credit": false,
+            "game": "BN6",
+            "preview": "bn6_securitycube.png"
+      },
+      {
+            "name": "Security Cube (gray)",
+            "files": [
+                  "bn6_cube_gray.tsx",
+                  "bn6_cube_gray.png"
+            ],
+            "author": "unknown",
+            "type": "object",
+            "credit": false,
+            "game": "BN6",
+            "preview": "bn6_cube_gray.png"
+      },
+      {
+            "name": "Home Warp",
+            "files": [
+                  "home warp.png",
+                  "home warp.tsx"
+            ],
+            "author": "unknown",
+            "type": "object",
+            "credit": false,
+            "game": "BN5",
+            "preview": "home warp.png"
+      },
+      {
+            "name": "Mystery Data",
+            "files": [
+                  "MysteryData.png",
+                  "MysteryData.tsx"
+            ],
+            "author": "unknown",
+            "type": "object",
+            "credit": false,
+            "game": "BN4.5",
+            "preview": "MysteryData.png"
+      },
+      {
+            "name": "Internet5-8 Area Tiles",
+            "files": [
+                  "ZeroAccountTileset.tsx",
+                  "ZeroAccountTileset.png"
+            ],
+            "author": "unknown",
+            "type": "tile",
+            "credit": false,
+            "game": "BN4.5",
+            "preview": "ZeroAccountTileset.png"
+      },
+      {
+            "name": "Area Warps",
+            "files": [
+                  "4_5Warps.png",
+                  "4_5Warps.tsx"
+            ],
+            "author": "unknown",
+            "type": "object",
+            "credit": false,
+            "game": "BN4.5",
+            "preview": "4_5Warps.png"
+      },
+      {
+            "name": "Alpha Comp Tiles",
+            "files": [
+                  "Alpha_Comp.png",
+                  "Alpha_Comp.tsx"
+            ],
+            "author": "unknown",
+            "type": "tile",
+            "credit": false,
+            "game": "BN3",
+            "preview": "Alpha_Comp.png"
+      },
+        {"name":"Oven Comp (custom)","files":["Cords.png","Cords.tsx"],"type":"tile","author":"unknown","credit":false,"game":"BN1","preview":"Cords.png"},
+        {"name":"Fireman Flames","files":["Fireman Flame.png","Fireman Flame.tsx"],"type":"object","author":"unknown","credit":false,"game":"BN1","preview":"Fireman Flame.png"},
+        {"name":"Oven Comp Honeycombs","files":["Oven Honeycombs.tsx","ovenhoneycomb.png"],"type":"tile","author":"unknown","credit":false,"game":"BN1","preview":"ovenhoneycomb.png"},
+        {"name":"Traffic Light Comp Arrows","files":["TrafficArrows.png","TrafficArrows.tsx"],"type":"tile","author":"unknown","credit":false,"game":"BN1","preview":"TrafficArrows.png"},
+        {"name":"Traffic Light Comp Floor","files":["TrafficFloor.png","TrafficFloor.tsx"],"type":"tile","author":"unknown","credit":false,"game":"BN1","preview":"TrafficFloor.png"},
+        {"name":"Square Tiles","files":["BN2SquareColorful.png","BN2SquareColorful.tsx"],"type":"tile","author":"unknown","credit":false,"game":"BN2","preview":"BN2SquareColorful.png"},
+        {"name":"Net Arrows","files":["Den_Exits.png","Den_Exits.tsx"],"type":"tile","author":"unknown","credit":false,"game":"BN2","preview":"Den_Exits.png"},
+        {"name":"Den Area Animated Overlays","files":["DenAreaTechAnimatedOverlay.png","DenAreaTechAnimatedOverlay.tsx"],"type":"tile","author":"unknown","credit":false,"game":"BN2","preview":"DenAreaTechAnimatedOverlay.png"},
+        {"name":"Netopia Arrows","files":["Exits Redux.png","Exits Redux.tsx"],"author":"unknown","credit":false,"game":"BN2","preview":"Exits Redux.png"},
+        {"name":"Square Transition Tile","files":["FlatRamp.png","FlatRamp.tsx"],"type":"tile","author":"unknown","credit":false,"game":"BN2","preview":"FlatRamp.png"},
+        {"name":"Raoul HP Tiles","files":["FloorTilesColorful.png","FloorTilesColorful.tsx"],"type":"tile","author":"unknown","credit":false,"game":"BN2","preview":"FloorTilesColorful.png"},
+        {"name":"Ice Tile Tops","files":["IceTileTopper.png","IceTileTopper.tsx"],"type":"tile","author":"unknown","credit":false,"game":"BN3","preview":"IceTileTopper.png"},
+        {"name":"Internet House Door","files":["Internet House.png","Internet House.tsx"],"type":"object","author":"unknown","credit":false,"game":"BN2","preview":"Internet House.png"},
+        {"name":"Netopia Tiles","files":["Netopia Tileset.png","Netopia Tileset.tsx"],"type":"tile","author":"unknown","credit":false,"game":"BN2","preview":"Netopia Tileset.png"},
+        {"name":"Netopia Conveyer Tile","files":["Netopia_Tileset_directional_paths.png","Netopia_Tileset_directional_paths.tsx"],"type":"tile","author":"unknown","credit":false,"game":"BN2","preview":"Netopia_Tileset_directional_paths.png"},
+        {"name":"Snow Tile Toppers","files":["SnowTileTopper.png","SnowTileTopper.tsx"],"type":"tile","author":"unknown","credit":false,"game":"BN3","preview":"SnowTileTopper.png"},
+        {"name":"Square Warps","files":["SquareWarpsA.png","SquareWarpsA.tsx"],"type":"object","author":"unknown","credit":false,"game":"BN2","preview":"SquareWarpsA.png"},
+        {"name":"Undernet Animated Tile Tops","files":["Bn2UndernetAnimatedTiles.tsx","Bn2UndernetAnimatedTiles.png"],"type":"tile","author":"unknown","credit":false,"game":"BN2","preview":"Bn2UndernetAnimatedTiles.png"},
+        {"name":"ACDC Arrows","files":["ACDC_Arrows.png","ACDC_Arrows.tsx"],"type":"tile","author":"unknown","credit":false,"game":"BN3","preview":"ACDC_Arrows.png"},
+        {"name":"Beach Conveyers","files":["Beach Arrows.png","Beach Arrows.tsx"],"type":"tile","author":"unknown","credit":false,"game":"BN3","preview":"Beach Arrows.png"},
+        {"name":"Beach Stairs","files":["Beach Stairs.png","Beach Stairs.tsx"],"type":"tile","author":"unknown","credit":false,"game":"BN3","preview":"Beach Stairs.png"},
+        {"name":"Beach Tiles","files":["Beach Tileset.png","Beach Tiles.tsx"],"type":"tile","author":"unknown","credit":false,"game":"BN3","preview":"Beach Tileset.png"},
+        {"name":"Beach_Next_Area","files":["Beach_Next_Area.png","Beach_Next_Area.tsx"],"type":"tile","author":"unknown","credit":false,"game":"BN3","preview":"Beach_Next_Area.png"},
+        {"name":"Beach Warp","files":["Beach_Warp.png","Beach_Warp.tsx"],"type":"object","type":"object","author":"unknown","credit":false,"game":"BN3","preview":"Beach_Warp.png"},
+        {"name":"ACDC Warp","files":["BN3ACDC1Warp.png","BN3ACDC1Warp.tsx"],"type":"object","author":"unknown","credit":false,"game":"BN3","preview":"BN3ACDC1Warp.png"},
+        {"name":"ACDC Square Warp","files":["BN3ACDCWarp2.png","BN3ACDCWarp2.tsx"],"type":"object","author":"unknown","credit":false,"game":"BN3","preview":"BN3ACDCWarp2.png"},
+        {"name":"Dex Panel","files":["BN3DexPanel.png","BN3DexPanel.tsx"],"type":"tile","author":"unknown","credit":false,"game":"BN3","preview":"BN3DexPanel.png"},
+        {"name":"Tamako Panel","files":["BN3TamakoPanel.png","BN3TamakoPanel.tsx"],"type":"tile","author":"unknown","credit":false,"game":"BN3","preview":"BN3TamakoPanel.png"},
+        {"name":"Yai Panel","files":["BN3YaiPanel.png","BN3YaiPanel.tsx"],"type":"tile","author":"unknown","credit":false,"game":"BN3","preview":"BN3YaiPanel.png"},
+        {"name":"Yoka Compression Roads","files":["CompressionRoadsYoka.png","CompressionRoadsYoka.tsx"],"type":"tile","author":"unknown","credit":false,"game":"BN3","preview":"CompressionRoadsYoka.png"},
+        {"name":"Cyber Rail","files":["Cyber_Rail.png","Cyber_Rail.tsx"],"type":"tile","author":"unknown","credit":false,"game":"BN3","preview":"Cyber_Rail.png"},
+        {"name":"DrillManPortal36x46","files":["DrillManPortal36x46.png","DrillManPortal36x46.tsx"],"type":"tile","author":"unknown","credit":false,"game":"BN3","preview":"DrillManPortal36x46.png"},
+        {"name":"BBS Board","files":["BBSOldFixed.png","BBSOldFixed.tsx"],"type":"object","author":"unknown","credit":false,"game":"BN3","preview":"BBSOldFixed.png"},
+        {"name":"BBS Board","files":["BBS-BN4.png","BBS-BN4.tsx"],"author":"unknown","credit":false,"game":"BN4","preview":"BBS-BN4.png"},
+        {
+            "name": "BBS Board",
+            "files": [
+                  "BBS.tsx",
+                  "BBS.png"
+            ],
+            "author": "unknown",
+            "type": "object",
+            "credit": false,
+            "game": "BN6",
+            "preview": "BBS.png"
+      },
+      {"name":"HP Tiles","files":["bn6_pc_tile.png","bn6_pc_tile.tsx"],"type":"tile","author":"unknown","credit":false,"game":"BN6","preview":"bn6_pc_tile.png"},
+
+      /*
+      ,
+      {
+            "name": "",
+            "files": [
+                  "",
+                  ""
+            ],
+            "author": "unknown",
+            "type": "object",
+            "credit": false,
+            "game": "",
+            "preview": ""
+      }
+      */
+    ];
