cec72b13657f11b1cc892068e56dc15f86c09330
Indiana.json <1751044+indianajson@users.noreply.github.com>
2025-05-30 15:30:34 -0400

    beta test

---
+++ backgrounds.js
@@ -0,0 +1,16 @@
+let allBackgrounds = [
+    {"name":"Papa's PC Comp","velx":"0.35","vely":"0.20","files":["papapc.animation","papapc.png"],"author":"Kirbyatina","credit":false,"game":"BN1","preview":"papapc.animation"},
+    {"name":"Clockman's Area","velx":"0.20","vely":"0.10","files":["clockman.animation","clockman.png"],"author":"Kirbyatina","credit":false,"game":"BN1","preview":"clockman.animation"},
+    {"name":"Dex's PC","velx":"0.10","vely":"0.05","files":["dex-pc.animation","dex-pc.png"],"author":"Kirbyatina","credit":false,"game":"BN1","preview":"dex-pc.animation"},
+    {"name":"Dex's PC (Battle)","velx":"0.10","vely":"0.05","files":["dex-pc-battle.animation","dex-pc-battle.png"],"author":"Kirbyatina","credit":false,"game":"BN1","preview":"dex-pc-battle.animation"},
+    {"name":"School Comp","velx":"0","vely":"0","files":["school-comp.png","school-comp.animation"],"author":"Kirbyatina","credit":false,"game":"BN1","preview":"school-comp.animation"},   
+    {"name":"Internet","velx":"0.35","vely":"0.20","files":["internet.png","internet.animation"],"author":"Kirbyatina","credit":false,"game":"BN1","preview":"internet.animation"},
+  /*  {"name":"Power Plant","velx":"0.0","vely":"0.0","files":["powerplant.animation","powerplant.png"],"author":"Kirbyatina","credit":false,"game":"BN1","preview":"powerplant.animation"}, */
+    {"name":"Power Plant (Battle)","velx":"0.0","vely":"0.0","files":["powerplant-battle.animation","powerplant-battle.png"],"author":"Kirbyatina","credit":false,"game":"BN1","preview":"powerplant-battle.animation"},
+
+/*
+
+    {"name":"","velx":"","vely":"","files":["",""],"author":"unknown","credit":false,"game":"","preview":""},
+
+*/
+];
---
+++ music.js
@@ -0,0 +1,9 @@
+let allMusic = [
+    {"name": "ACDC (Overworld)","files": ["ACDC Town-BN1.ogg"], "author": "D3str0y3d","credit": false,"game": "BN1","duration": "1:58"},
+    {"name": "School (Overworld)","files": ["School-BN1.ogg"], "author": "D3str0y3d","credit": false,"game": "BN1","duration": "1:28"},
+    {"name": "Internet Theme","files": ["Internet Theme-BN1.ogg"], "author": "Indiana","credit": false,"game": "BN1","duration": "1:39"},
+    {"name": "School Comp","files": ["School Comp-BN1.ogg"], "author": "Indiana","credit": false,"game": "BN1","duration": "1:49"},
+    {"name": "Oven Comp","files": ["Oven Comp-BN1.ogg"], "author": "Indiana","credit": false,"game": "BN1","duration": "1:53"},
+    {"name": "Waterworks Comp","files": ["Waterworks-BN1.ogg"], "author": "D3str0y3d","credit": false,"game": "BN1","duration": "1:57"},
+    {"name": "Traffic Light Comp","files": ["Traffic Light Comp-BN1.ogg"], "author": "D3str0y3d","credit": false,"game": "BN1","duration": "1:27"},
+    ];
