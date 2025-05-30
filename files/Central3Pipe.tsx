97258857e4bc514cf608f54650b07a1953d6541f
Indiana.json <1751044+indianajson@users.noreply.github.com>
2025-05-30 15:33:28 -0400

    beta test

---
+++ files/Central3Pipe.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Central3Pipe" tilewidth="40" tileheight="48" tilecount="1" columns="1">
+ <tileoffset x="-6" y="6"/>
+ <image source="Central3Pipe.png" width="40" height="48"/>
+</tileset>
