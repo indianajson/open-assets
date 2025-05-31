6a1e2c610d9e3f3a3adecdb01b773132c86d86b2
Indiana.json <1751044+indianajson@users.noreply.github.com>
2025-05-31 16:47:42 -0400

    fix typo

--- files/Secret_Area_Warp.tsx
+++ files/Secret_Area_Warp.tsx
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.1" name="Secret Area Warp" tilewidth="80" tileheight="43" tilecount="3" columns="3" objectalignment="top">
  <tileoffset x="0" y="-8"/>
- <image source="Secret Area Warp.png" width="240" height="43"/>
+ <image source="Secret_Area_Warp.png" width="240" height="43"/>
  <tile id="0">
   <animation>
    <frame tileid="0" duration="2000"/>
