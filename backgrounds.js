8797f1f88972f8715d9b599bb40ab643e99d7999
Indiana.json <1751044+indianajson@users.noreply.github.com>
2025-06-01 12:48:46 -0400

    fixes

--- backgrounds.js
+++ backgrounds.js
@@ -1,5 +1,5 @@
 let allBackgrounds = [
-    {"name":"Papa's PC Comp","velx":"0.35","vely":"0.20","files":["papapc.animation","papapc.png"],"author":"Kirbyatina","credit":false,"game":"BN1","preview":"papapc.animation"},
+    {"name":"Sal's PC Comp","velx":"0.35","vely":"0.20","files":["papapc.animation","papapc.png"],"author":"Kirbyatina","credit":false,"game":"BN1","preview":"papapc.animation"},
     {"name":"Clockman's Area","velx":"0.20","vely":"0.10","files":["clockman.animation","clockman.png"],"author":"Kirbyatina","credit":false,"game":"BN1","preview":"clockman.animation"},
     {"name":"Dex's PC","velx":"0.10","vely":"0.05","files":["dex-pc.animation","dex-pc.png"],"author":"Kirbyatina","credit":false,"game":"BN1","preview":"dex-pc.animation"},
     {"name":"Dex's PC (Battle)","velx":"0.10","vely":"0.05","files":["dex-pc-battle.animation","dex-pc-battle.png"],"author":"Kirbyatina","credit":false,"game":"BN1","preview":"dex-pc-battle.animation"},
