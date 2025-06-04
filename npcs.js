16eafbd33a62813d108f6c87a5b03a996e5d4224
Indiana JSON <1751044+indianajson@users.noreply.github.com>
2025-06-03 23:39:44 -0400

    new tabs

---
+++ npcs.js
@@ -0,0 +1,14 @@
+let allNPCs = [
+    {"name":"Mr. Prog (Santa)","files":["progsmas.animation","progsmas.png"],"author":"unknown","game":"Custom","preview":"progsmas.animation"},
+    {"name":"Mettaur Swing","files":["MetSwing.animation","MetSwing.png"],"author":"D3str0y3d","game":"Custom","preview":"MetSwing.animation"},
+    {"name":"Bass","files":["bass.animation","bass.png"],"author":"unknown","game":"BN6","preview":"bass.animation"},
+
+
+    
+/*
+    //this is an empty dictionary you can use to make new entries
+
+    {"name":"","files":["",""],"author":"","game":"","preview":""},
+
+*/
+];
---
+++ tools.js
@@ -0,0 +1,12 @@
+let allTools = [
+    {"name": "Spritesheet Editor","description":'A tool designed to allow you to edit or create .animation or .TSX files for spritesheets this webtool has all the major functionality to get you from sprite to ONB asset in a matter of minutes. <br><br> <button><a href="https://keristero.github.io/spritesheet-tool/" target="_blank" style="text-decoration:none; color:#000;">Launch Tool</a></button>',"author": "Keristero","URL":"","repository":""},
+
+    /*
+    
+    //this is an empty dictionary you can use to make new entries, it includes the code for the button in the description field. 
+
+
+    {"name": "","description":'&nbsp; <button id="download-single-tool"><a href="" target="_blank" style="text-decoration:none; color:#000;"></a></button>',"author": "","URL":"","repository":""},
+    */
+
+    ];
