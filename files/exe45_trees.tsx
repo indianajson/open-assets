3dab47ce8c5ac271698c9a104b74e4536bd4b1d6
Indiana JSON <1751044+indianajson@users.noreply.github.com>
2025-06-10 10:58:02 -0400

    fixed trees

--- files/exe45_trees.tsx
+++ files/exe45_trees.tsx
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.5" name="exe45 trees" tilewidth="64" tileheight="80" tilecount="4" columns="4" objectalignment="bottom">
  <tileoffset x="0" y="16"/>
- <image source="exe45 trees.png" width="256" height="80"/>
+ <image source="exe45_trees.png" width="256" height="80"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
    <object id="1" x="32" y="80">
