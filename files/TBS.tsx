2df4f101da42623b3b705977fc2eefa85e67d083
Indiana JSON <1751044+indianajson@users.noreply.github.com>
2025-07-06 22:29:33 -0400

    Added Tournament BBS

---
+++ files/TBS.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.11.2" name="TBS" tilewidth="37" tileheight="57" tilecount="1" columns="1" objectalignment="top">
+ <tileoffset x="3" y="4"/>
+ <grid orientation="isometric" width="64" height="32"/>
+ <transformations hflip="1" vflip="0" rotate="0" preferuntransformed="0"/>
+ <image source="TBS.png" width="37" height="57"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-1.5" y="11.5" width="21.5" height="37"/>
+  </objectgroup>
+ </tile>
+</tileset>
