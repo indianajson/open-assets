3f25ef8812e38ad338756a5fd3a302d859cc9b34
Indiana JSON <1751044+indianajson@users.noreply.github.com>
2025-07-09 15:10:34 -0400

    Fixed Noise Wave game

--- tiles.js
+++ tiles.js
@@ -145,7 +145,7 @@ let allData = [
  { "name": "Glow Tileset - Small", "files": [ "GlowSm.png", "GlowSm.tsx" ], "author": "unknown", "game": "Custom", "preview": "GlowSm.png", "id": "glow-tileset-small-custom" },
  { "name": "Glow Tileset - Stairs", "files": [ "GlowStairs.png", "GlowStairs.tsx" ], "author": "unknown", "game": "Custom", "preview": "GlowStairs.png", "id": "glow-tileset-stairs-custom" },
  { "name": "Holo Tile (BN6)", "files": [ "HoloPanelz.tsx", "HoloPanelz.png" ], "author": "Rune", "game": "Custom", "preview": "HoloPanelz.png", "id": "holo-tile-bn6-custom" },
- { "name": "Noise Wave Entrance", "files": [ "NoiseWaveEntrance.png", "NoiseWaveEntrance.tsx" ], "author": "FrozenLake", "game": "Other", "preview": "NoiseWaveEntrance.png", "id": "noise-wave-entrance-other" },
+ { "name": "Noise Wave Entrance", "files": [ "NoiseWaveEntrance.png", "NoiseWaveEntrance.tsx" ], "author": "FrozenLake", "game": "SSR3/SF3", "preview": "NoiseWaveEntrance.png", "id": "noise-wave-entrance-other" },
  { "name": "Shademan Comp Warp Animation", "files": [ "ShademanCompWarpAnimation.png", "ShademanCompWarpAnimation.tsx" ], "author": "unknown", "game": "EXE4/BN4", "preview": "ShademanCompWarpAnimation.png", "id": "shademan-comp-warp-animation-exe4" },
  { "name": "Undernet Statue", "type": "object", "files": [ "UndernetStatue.png", "UndernetStatue.tsx" ], "author": "unknown", "game": "EXE3/BN3", "preview": "UndernetStatue.png", "id": "undernet-statue-exe3" },
  { "name": "Flame Door", "files": [ "SDoor.png", "SDoor.tsx" ], "author": "unknown", "game": "Other", "preview": "SDoor.png", "id": "flame-door-other" },
