0a24bd380fb60ee3c252b13004022e3fc4240ab0
Indiana JSON <1751044+indianajson@users.noreply.github.com>
2025-07-15 22:27:40 -0400

    Added Water Tiles

---
+++ files/water_tiles.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="water_tiles" tilewidth="64" tileheight="54" tilecount="4" columns="4">
+ <tileoffset x="0" y="24"/>
+ <image source="water_tiles.png" width="256" height="54"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="2" duration="200"/>
+   <frame tileid="3" duration="200"/>
+   <frame tileid="2" duration="200"/>
+   <frame tileid="1" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
