a1192b5c8dca4abfc712fd4dc0e368d602e3c1b2
Indiana JSON <1751044+indianajson@users.noreply.github.com>
2025-07-16 00:14:59 -0400

    Added Square Data

---
+++ files/square_data.tsx
@@ -0,0 +1,91 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.11.2" name="Square Data" tilewidth="16" tileheight="40" tilecount="48" columns="16">
+ <properties>
+  <property name="Solid" type="bool" value="true"/>
+ </properties>
+ <image source="square_data.png" width="256" height="120"/>
+ <tile id="0">
+  <properties>
+   <property name="Solid" type="bool" value="true"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0" y="32" width="16" height="10">
+    <ellipse/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="11" duration="100"/>
+   <frame tileid="12" duration="100"/>
+   <frame tileid="13" duration="100"/>
+   <frame tileid="14" duration="100"/>
+   <frame tileid="15" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="Solid" type="bool" value="true"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="5" x="0" y="32" width="16" height="10">
+    <ellipse/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="16" duration="100"/>
+   <frame tileid="17" duration="100"/>
+   <frame tileid="18" duration="100"/>
+   <frame tileid="19" duration="100"/>
+   <frame tileid="20" duration="100"/>
+   <frame tileid="21" duration="100"/>
+   <frame tileid="22" duration="100"/>
+   <frame tileid="23" duration="100"/>
+   <frame tileid="24" duration="100"/>
+   <frame tileid="25" duration="100"/>
+   <frame tileid="26" duration="100"/>
+   <frame tileid="27" duration="100"/>
+   <frame tileid="28" duration="100"/>
+   <frame tileid="29" duration="100"/>
+   <frame tileid="30" duration="100"/>
+   <frame tileid="31" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="Solid" type="bool" value="true"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="3" x="0" y="32" width="16" height="10">
+    <ellipse/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="32" duration="100"/>
+   <frame tileid="33" duration="100"/>
+   <frame tileid="34" duration="100"/>
+   <frame tileid="35" duration="100"/>
+   <frame tileid="36" duration="100"/>
+   <frame tileid="37" duration="100"/>
+   <frame tileid="38" duration="100"/>
+   <frame tileid="39" duration="100"/>
+   <frame tileid="40" duration="100"/>
+   <frame tileid="41" duration="100"/>
+   <frame tileid="42" duration="100"/>
+   <frame tileid="43" duration="100"/>
+   <frame tileid="44" duration="100"/>
+   <frame tileid="45" duration="100"/>
+   <frame tileid="46" duration="100"/>
+   <frame tileid="47" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
